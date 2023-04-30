bff6863e157f0122535fdfbbfd0c5db100018ad8
Rihanul Islam <rihanthedev@gmail.com>
2023-04-30 15:53:08 +0600

    data created

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 
 const place = require('./AllData/placeInfo.json')
+const blog = require('./AllData/blog.json')
+const news = require('./AllData/news.json')
 
 app.get('/',(req,res)=>{
     res.send('Welcome To Traval guru Server. Your Server Is Running')
@@ -14,6 +16,14 @@ app.get('/place',(req,res)=>{
     res.send(place)
 })
 
+// blog infromation
+app.get('/blog',(req,res)=>{
+    res.send(blog)
+})
+// news
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
 app.listen(port,()=>{
     console.log('Your Server is Running On Port: ',port);
 })
