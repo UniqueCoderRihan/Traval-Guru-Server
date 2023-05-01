79042ab4b4882ac8346aaa02dbebc725b99bae45
Rihanul Islam <rihanthedev@gmail.com>
2023-05-01 10:53:54 +0600

    Cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 3000;
+
+app.use(cors)
 
 const place = require('./AllData/placeInfo.json')
 const blog = require('./AllData/blog.json')
